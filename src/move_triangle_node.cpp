a018f9a56dff7ac4294a857f479b776796633701
matthijs040 <matthijsbesselink@live.nl>
2019-11-22 14:59:44 +0100

    cleaned up the move_node code after fixing the subpub class usage

--- src/move_triangle_node.cpp
+++ src/move_triangle_node.cpp
@@ -17,41 +17,30 @@
 
 #include "turtlebot3_example/Turtlebot3Goal.h"
 
-// A global copy of the newest triangle is nescesary if you want to test its value in main()
-// assignment5::Triangle triangle = assignment5::Triangle();
-
-
-    ros::Publisher goalPub;
-
-// Callback to respond to the triangle message sent on the cmd topic.
-void cmdCallback(const assignment5::TriangleConstPtr&  msg )
-{
-    auto goal = turtlebot3_example::Turtlebot3ActionGoal();
-    goal.goal.goal.x = msg->sideLength;
-    goalPub.publish(goal);
-}
-
-
 const turtlebot3_example::Turtlebot3ActionGoal cmdHandler( const assignment5::TriangleConstPtr& msg )
 {
+    ROS_INFO("Received Triangle message.");
+
     turtlebot3_example::Turtlebot3ActionGoal action = turtlebot3_example::Turtlebot3ActionGoal();
     action.goal.goal.x = msg->sideLength;
+    action.goal.goal.y = msg->sideLength;
+    action.goal.goal.z = msg->sideLength;
+
+    
 
     auto result = turtlebot3_example::Turtlebot3ActionGoal(action);
     return result;
 }
 
-
-
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "move_triangle_node");
     ros::NodeHandle n = ros::NodeHandle();
-
-    ros::Subscriber cmdSub = n.subscribe("/cmd", 1, cmdCallback );
     
     SubscriptionPublisher<const turtlebot3_example::Turtlebot3ActionGoal, const assignment5::TriangleConstPtr&> 
     sp = SubscriptionPublisher("/turtlebot3/goal", "/cmd", std::function(cmdHandler) );
+
+    ROS_INFO("Awaiting a Triangle message on the /cmd topic.");
 
     ros::spin();
     
